7ea21a9ba4497eb3a948d3f23643436d4ec02896
Alfredo Supanta <alfredo.supanta.26@gmail.com>
2022-02-25 23:57:06 -0300

    terminado

--- index.js
+++ index.js
@@ -1,24 +1,72 @@
 $(function(){
     $('#formulario').submit(function(evento){
         const nombre = $('#nombre');
-        const password = $('#password');
+        const apellido = $('#apellido')
+        const rut = $('#rut')
+        const direccion = $('#direccion')
+        const comuna = $('#comuna')
+        const vehiculo = $('#vehiculo')
+        const marca = $('#marca')
+        const modelo = $('#modelo')
+        const anio = $('#anio')
+        const si = $('#si')
+        const no = $('#no')
+        const servicio = $('#servicio')
+
+
         const valorNombre = nombre.val()
+        const valorApellido = apellido.val()
+        const valorRut = rut.val()
+        const valorDireccion = direccion.val()
+        const valorComuna = comuna.val()
+        const valorVehiculo = vehiculo.val()
+        const valorMarca = marca.val()
+        const valorModelo = modelo.val()
+        const valorAnio = anio.val()
+        const valorSi = si.val()
+        const valorNo = no.val()
+        const valorServicio = servicio.val()
+
 
         const radio = $("input[type='radio']:checked").length;
         const alertaRadio = $('#alertaRadio');
         
         
+        const alertaNombre = $('#alertaNombre');
+        const alertaApellido = $('#alertaApellido');
+        const alertaRut = $('#alertaRut')
 
-
-        const alertaInfo = $('#alertaInfo');
-
-        /* if (valorNombre == null || valorNombre ==''){
+        if (valorNombre == null || valorNombre =='' || valorNombre == 0 || valorNombre == 1 || valorNombre == 2 || valorNombre == 3 || valorNombre == 4 || valorNombre == 5 || valorNombre == 6){
             evento.preventDefault();
-            alertaInfo.addClass("alert alert-danger");
+            alertaNombre.addClass("alert alert-danger");
             
-            alertaInfo.text("Debes rellenar el campo");
-            console.log("hola que tal")
-        } */
+            alertaNombre.text("Debes rellenar el campo, solo con letras");
+            
+        }
+
+        if (valorApellido == null || valorApellido =='' || valorApellido == 0 || valorApellido == 1 || valorApellido == 2 || valorApellido == 3 || valorApellido == 4 || valorApellido == 5 || valorApellido == 6){
+            evento.preventDefault();
+            alertaApellido.addClass("alert alert-danger");
+            
+            alertaApellido.text("Debes rellenar el campo, solo con letras");
+            
+        }
+
+        if (valorRut == null || valorRut =='' ){
+            evento.preventDefault();
+            alertaRut.addClass("alert alert-danger");
+            
+            alertaRut.text("Debes rellenar el campo, solo con números");
+            
+        }
+
+
+
+
+
+
+
+
 
         if (radio == 0){
             evento.preventDefault();
